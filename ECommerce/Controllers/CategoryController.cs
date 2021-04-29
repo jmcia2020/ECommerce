902085b093e43c67f180aca3fb08bd273c20b5ab
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-28 23:28:09 -0500

    cleanup

--- ECommerce/Controllers/CategoryController.cs
+++ ECommerce/Controllers/CategoryController.cs
@@ -1,67 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using ECommerce.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace ECommerce.Controllers
 {
-    public class ProductsController : Controller
+    public class CategoryController : Controller
     {
-        // GET: ProductsController
+        // GET: CategoryController
         public ActionResult Index()
         {
-            var products = new[]{
-                new Product
-                {
-                    Id = 1,
-                    Name = "Easter Pillow Cover",
-                    Description = "Burlap 18 X 18 pillow cover with Easter motif",
-                    
-                },
-                new Product
-                {
-                    Id = 2,
-                    Name = "Summer Pillow Cover",
-                    Description = "Burlap 18 X 18 pillow cover with Summer Flowers motif",
-                    
-                },
-
-                new Product
-                {
-                    Id = 3,
-                    Name = "Throw Blanket",
-                    Description = "Yellow 100% Cotton 50\" x 60\" throw blanket",
-
-                },
-
-                new Product
-                {
-                    Id = 4,
-                    Name = "Down Alternative Bed Pillow",
-                    Description = "Standard size machine washable medium firm down alternative pillow",
-
-                },
-            };
-            return View(products);
+            return View();
         }
 
-        // GET: ProductsController/Details/5
+        // GET: CategoryController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: ProductsController/Create
+        // GET: CategoryController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: ProductsController/Create
+        // POST: CategoryController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -76,13 +42,13 @@ namespace ECommerce.Controllers
             }
         }
 
-        // GET: ProductsController/Edit/5
+        // GET: CategoryController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: ProductsController/Edit/5
+        // POST: CategoryController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -97,13 +63,13 @@ namespace ECommerce.Controllers
             }
         }
 
-        // GET: ProductsController/Delete/5
+        // GET: CategoryController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: ProductsController/Delete/5
+        // POST: CategoryController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
--- ECommerce/Controllers/ProductController.cs
+++ ECommerce/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace ECommerce.Controllers
 {
@@ -12,7 +14,39 @@ namespace ECommerce.Controllers
         // GET: ProductController
         public ActionResult Index()
         {
-            return View();
+            var products = new[]{
+                new Product
+                {
+                    Id = 1,
+                    Name = "Easter Pillow Cover",
+                    Description = "Burlap 18 X 18 pillow cover with Easter motif",
+                    
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Summer Pillow Cover",
+                    Description = "Burlap 18 X 18 pillow cover with Summer Flowers motif",
+                    
+                },
+
+                new Product
+                {
+                    Id = 3,
+                    Name = "Throw Blanket",
+                    Description = "Yellow 100% Cotton 50\" x 60\" throw blanket",
+
+                },
+
+                new Product
+                {
+                    Id = 4,
+                    Name = "Down Alternative Bed Pillow",
+                    Description = "Standard size machine washable medium firm down alternative pillow",
+
+                },
+            };
+            return View(products);
         }
 
         // GET: ProductController/Details/5
