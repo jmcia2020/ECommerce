7980666d750b21642b808fab1237591c610b735f
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-28 21:27:39 -0500

    scaffolded Controllers and Views for Admin, Categories and Products

--- ECommerce/Models/Admin.cs
+++ ECommerce/Models/Admin.cs
@@ -13,6 +13,10 @@ namespace ECommerce.Models
 
         public string LastName { get; set; }
 
+        public string UserName { get; set; }
 
+        public string Email { get; set; }
+
+        public string Password { get; set; }
     }
 }
