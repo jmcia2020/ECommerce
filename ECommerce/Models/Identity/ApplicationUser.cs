a59f0ce72e063759413054b2d08f7f98d7c6dfea
jmcia2020 <jeanmmcmahon@gmail.com>
2021-05-02 20:28:53 -0500

    successful migration. Updated Database.

--- ECommerce/Models/Identity/ApplicationUser.cs
+++ ECommerce/Models/Identity/ApplicationUser.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +7,5 @@ namespace ECommerce.Models.Identity
 {
     public class ApplicationUser : IdentityUser<int>
     {
-    }
-
-    public class ApplicationRole : IdentityRole<int>
-    {
-        DateTime? CreatedAt { get; set; }
     }
 }
