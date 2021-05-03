4cc8239b6bc8c9e2ec87356e5be17c13e4457dd9
jmcia2020 <jeanmmcmahon@gmail.com>
2021-05-02 19:22:22 -0500

    added Microsoft.EntityFrameworkCore.SqlServer

--- ECommerce/Startup.cs
+++ ECommerce/Startup.cs
@@ -1,3 +1,4 @@
+using ECommerce.DbData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,13 @@ namespace ECommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var cs = Configuration.GetConnectionString("DefaultConnection");
+                if (cs == null) throw new InvalidOperationException("DefaultConnection is missing!");
+
+                options.UseSqlServer(cs);
+            });
             services.AddControllersWithViews();
         }
 
