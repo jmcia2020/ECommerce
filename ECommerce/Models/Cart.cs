453976cd23af884f905a34a152ce25f9f69d9073
jmcia2020 <jeanmmcmahon@gmail.com>
2021-05-04 20:23:13 -0500

    created cart in RazorPages

---
+++ ECommerce/Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class Cart
+    {
+        [StringLength(250)]
+        public string ProdImageUrl { get; set; }
+        
+        public int Cart_Id { get; set; }
+        
+        public string Item_Id { get; set; }
+
+        public int Qty { get; set; }
+
+        /*
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool OnSale { get; set; }
+
+
+
+        // Foreign Key
+        // Nullable so it's not required
+        public int? Category_Id { get; set; }
+
+        // Navigation Property
+        public Category Category { get; set; }
+        */
+    }
+}
